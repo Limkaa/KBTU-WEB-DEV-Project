6409ab206857ffff5f153eb0c4813b63fd4cb9d2
Limkaa <khamraevalim@gmail.com>
2021-04-10 01:05:52 +0600

    Created payment page

--- Frontend/src/app/payments/payments.component.ts
+++ Frontend/src/app/payments/payments.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentsComponent implements OnInit {
 
+  plans = [25000,50000,75000,150000,300000];
+  paymentAmount!: number;
+  paymentPlan: number = 2;
+  paymentMethod: number = 1;
   constructor() { }
 
   ngOnInit(): void {
+  	this.paymentAmount = this.plans[this.paymentPlan-1];
   }
 
+  onPaymentPlanChange(event: any){
+  	this.paymentPlan = event.target.value;
+  	this.paymentAmount = this.plans[this.paymentPlan-1];
+  }
+
+  onPaymentMethodChange(event: any){
+  	this.paymentMethod = event.target.value;
+  }
+
+  paymentProcess() {
+  	console.log(this.paymentPlan)
+  	console.log(this.paymentMethod)  
+  }
 }
